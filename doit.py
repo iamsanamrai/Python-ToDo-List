cef657a7e31f1b1fc303d37deac05ff0a11dc9b9
sawnum <lonelykan6a@gmail.com>
2023-07-30 22:48:08 +0530

    feature added

--- doit.py
+++ doit.py
@@ -8,5 +8,23 @@ todos = [{"todo": "Task for HNH", "done": False}]
 def index():
     return render_template("index.html", todos=todos)
 
+
+@app.route("/add", methods=["POST"])
+def add():
+    todo = request.form['todo']
+    todos.append({"task": todo, "done": False})
+    return redirect(url_for("index"))
+
+
+@app.route("/edit/<init:index>", methods=["GET", "POST"])
+def edit(index):
+    todo = todos[index]
+    if request.method == "POST":
+        todo['task'] = request.form["todo"]
+        return redirect(url_for(index))
+    else:
+        return render_template("edit.html", todo=todo, index=index)
+
+
 if __name__ == '__main__':
     app.run(debug=True) 
